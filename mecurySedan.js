8f85f039c9826ae69dca7db8f13ab9ac25fe3d50
malfell <malfellows13@gmail.com>
2022-04-23 22:11:47 -0500

    imported vehicle.js to mercurySedan & created car class

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,23 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+//Imports vehicle.js and the Vehicle class
+const VehicleModule = require("./vehicle").Vehicle
+
+class Car extends VehicleModule {
+  //need constructor parameters from parent class
+  constructor(make, model, year, color, mileage){
+  //need super to call the parameters from parent class
+  super(make, model, year, color, mileage)
+    //new properties for car class
+    this.maximumPassengers = 5; 
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maximumSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
+}
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
